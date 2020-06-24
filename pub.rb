bd0adc491a515fdddc3e78401900ec34a146373f
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-06-24 11:57:49 +0100

    customer age test written and passed

--- pub.rb
+++ pub.rb
@@ -3,7 +3,6 @@ class Pub
     attr_reader(:name, :till, :drinks)
     attr_writer(:till)
 
-
     def initialize(name, till, drinks)
         @name = name
         @till = till
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -9,7 +9,7 @@ require_relative('../drink.rb')
 class TestCustomer < Minitest::Test
 
     def setup()
-        @customer = Customer.new("Tomek", 20)
+        @customer = Customer.new("Tomek", 20, 27)
         @drink = Drink.new("coke", 1)
         @pub = Pub.new("The Roseleaf", 100, [@drink])
     end
@@ -20,6 +20,10 @@ class TestCustomer < Minitest::Test
 
     def test_has_wallet()
         assert_equal(20, @customer.wallet)
+    end
+
+    def test_has_age()
+        assert_equal(27, @customer.age)
     end
 
     def test_buy_drink
